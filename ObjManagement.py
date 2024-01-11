31b1bf2b4939ca26bd2092edb965111c7b83aa2a
YSpiegel <spiegel.yoav@gmail.com>
2024-01-11 09:14:22 +0200

    Ver 1.27 - Design gem notes inside vault-page

--- ObjManagement.py
+++ ObjManagement.py
@@ -1,3 +1,40 @@
+import ctypes
+
+
+# Windows API text measurement
+class Size(ctypes.Structure):
+    _fields_ = [("cx", ctypes.c_long), ("cy", ctypes.c_long)]
+
+
+hdc = ctypes.windll.user32.GetDC(0)
+GetTextExtentPoint32 = ctypes.windll.gdi32.GetTextExtentPoint32W
+
+
+def string_preview(text, max_width_rems, font_height_pixels):
+    fontsize = font_height_pixels / 1.5
+    max_width_pixels = max_width_rems * fontsize
+
+    # Gradually remove characters until preview fits width
+    preview = text
+    while len(preview) > 1:
+        csize = Size()
+        GetTextExtentPoint32(hdc, preview, len(preview), ctypes.byref(csize))
+
+        if csize.cx < max_width_pixels:
+            break
+
+        preview = preview[:-1]
+
+    # Throw last space
+    if preview[-1] == " ":
+        preview = preview[:-1]
+
+    # Add ellipses
+    if preview != text:
+        preview += '...'
+
+    return preview
+
 
 class Vault:
     def __init__(self, title, user, description, type):
@@ -20,6 +57,8 @@ class Gem:
         self.user = user
         self.title = title
         self.content = content
+
+        self.preview = string_preview(self.content, 14, 21)
 
     def __str__(self):
         return f"{self.vault}|||{self.user}|||{self.title}|||{self.content}"
